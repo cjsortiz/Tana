41e2e877f5bd4eb1391cdfac825d275c47986579
unknown <celmarortiz98@yahoo.com>
2025-11-25 08:58:09 +0800

    first commit

---
+++ src/main/java/com/tana/tana_auth/TanaAuthApplication.java
@@ -0,0 +1,13 @@
+package com.tana.tana_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TanaAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TanaAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tana/tana_auth/TanaAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tana.tana_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TanaAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
